c4a253cbfa90d1d105e65aef0bbfbca4a26b28a7
analisisangka <hendrasmartfrenaja@gmail.com>
2026-03-06 04:13:04 +0800

    Add website files

--- script.js
+++ script.js
@@ -84,3 +84,52 @@ let angka=Math.floor(Math.random()*100);
 document.getElementById("random").innerHTML="Angka acak: "+angka;
 
 }
+
+function analisa(){
+
+let data = [
+n1.value,n2.value,n3.value,n4.value,n5.value,
+n6.value,n7.value,n8.value,n9.value,n10.value
+];
+
+let hitung = {0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0};
+
+data.forEach(num=>{
+num.split("").forEach(d=>{
+if(hitung[d] !== undefined){
+hitung[d]++;
+}
+});
+});
+
+let urut = Object.entries(hitung).sort((a,b)=>b[1]-a[1]);
+
+let hot = urut.slice(0,4).map(x=>x[0]);
+let cold = urut.slice(-4).map(x=>x[0]);
+
+document.getElementById("hot").innerHTML =
+"🔥 Angka Panas: "+hot.join(" - ");
+
+document.getElementById("cold").innerHTML =
+"❄️ Angka Dingin: "+cold.join(" - ");
+
+let prediksi=[];
+
+for(let i=0;i<20;i++){
+
+let angka="";
+
+for(let j=0;j<4;j++){
+
+angka += hot[Math.floor(Math.random()*hot.length)];
+
+}
+
+prediksi.push(angka);
+
+}
+
+document.getElementById("prediksi").innerHTML =
+"<br>Prediksi:<br>"+prediksi.join("<br>");
+
+}
